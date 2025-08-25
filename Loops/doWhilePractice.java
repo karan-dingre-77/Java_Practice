2ab711775ce60ce946c33e33de7d70e49813b27c
karan dingre 77 <karan.dingre77@gmail.com>
2025-08-25 23:52:19 +0530

    add new line

--- Loops/doWhilePractice.java
+++ Loops/doWhilePractice.java
@@ -6,7 +6,8 @@ public class doWhilePractice {
         System.out.println("we are printing numbers from 1 - 10 , using do while loop");
         System.out.println("hello i have learn git hub");
         System.out.println("i am karan hello");
-        System.out.println(hello);
+        System.out.println("hello");
+
         do{
             System.out.println(i);
             i++;
