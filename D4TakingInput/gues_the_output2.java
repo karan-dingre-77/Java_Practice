f251611f1bc707656aa9da2e2ae294341cd259a8
karan dingre 77 <karan.dingre77@gmail.com>
2025-08-27 00:51:57 +0530

    added Day 2

--- D4TakingInput/gues_the_output2.java
+++ D4TakingInput/gues_the_output2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class gues_the_output2 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter charecter ");
         String p = sc.next();                   //This program will through error of mismatchexeption
         int q = sc.nextInt();
 
