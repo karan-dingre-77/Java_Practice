bf7300c8c285ecaca82b5b427e0aa7b2e6a17e33
karan dingre 77 <karan.dingre77@gmail.com>
2025-08-27 16:02:56 +0530

    new changes

--- PatterPrinting/firstPattern.java
+++ PatterPrinting/firstPattern.java
@@ -1,8 +1,13 @@
-package PatterPrinting;
-
 public class firstPattern {
     public static void main(String[] args) {
-        System.out.println("hello");
-    }
+        int n = 5;  // number of rows
 
+        for (int i = 1; i <= n; i++) {
+            // print stars in each row
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(); // new line after each row
+        }
+    }
 }
