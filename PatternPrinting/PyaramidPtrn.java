5bfe12fa180f13258fa09efde4e6f5eb468f6c66
karan-dingre-77 <karan.dingre77@gmail.com>
2025-08-28 01:24:22 +0530

    Added new programs of Pattern printing

--- PatternPrinting/PyaramidPtrn.java
+++ PatternPrinting/PyaramidPtrn.java
@@ -1,6 +1,6 @@
 package PatternPrinting;
 import java.util.Scanner;
-public class PyradimicPtrn {
+public class PyaramidPtrn {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int r = sc.nextInt();
