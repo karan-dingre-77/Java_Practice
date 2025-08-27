13a1d6686874b35819f37fe6b06af225482945da
karan dingre 77 <karan.dingre77@gmail.com>
2025-08-27 16:19:40 +0530

    new changes

---
+++ ProblemsOnLoops/countNumOfdigit.java
@@ -0,0 +1,16 @@
+package ProblemsOnLoops;
+import java.util.Scanner;
+public class countNumOfdigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number here : ");
+        int num = sc.nextInt();
+        int count = 0;
+        for(    ; num>0 ; num+=0){
+            count++;
+            num=num/10;
+
+        }
+        System.out.println(count);
+    }
+}
---
+++ ProblemsOnLoops/sumOffdigit.java
@@ -0,0 +1,18 @@
+package ProblemsOnLoops;
+import java.util.Scanner;
+public class sumOffdigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number");
+        int num = sc.nextInt();
+        int ns = 0;
+        int an = 0;
+
+        for( ; num>0 ; num+=0){
+            ns = num%10;
+            an += ns;
+            num = num/10;
+        }
+        System.out.println(an);
+    }
+}
